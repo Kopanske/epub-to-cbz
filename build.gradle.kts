84def62172052461559412f70e9c700277e6e976
kopanske <martin.kopanske@hotmail.de>
2026-04-04 12:10:15 +0200

    Fisrt code

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import nl.littlerobots.vcu.plugin.resolver.VersionSelectors
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.version.catalog.update)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.shadow)
+    application
+}
+
+group = "com.github.kopanske"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation(libs.kotest.framework.engine)
+    testImplementation(libs.kotest.property)
+    testImplementation(libs.kotest.runner)
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+versionCatalogUpdate {
+    versionSelector(VersionSelectors.STABLE)
+}
+
+application {
+    mainClass = "org.example.MainKt"
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("ePubToCbz")
+    archiveVersion.set("1.0.0")
+    archiveClassifier.set("all")
+
+    mergeServiceFiles()
+}
